44d244c23b35bc6f167b32e2dba64b3c4f8cdcf5
Patrick Boynton <dakilangmanok@gmail.com>
2017-07-17 13:44:44 -0400

    Changed the single line comments to multi line comments. It just looks a bit nicer.

--- WebScraper.py
+++ WebScraper.py
@@ -1,15 +1,16 @@
 from lxml import html
 import requests
-
-#The lxml bits are not my own, however, the Scraper function is
-#This was written in part with a website that had a tutorial on
-#scraping with lxml, and I found it to be the easiest option I tried
-#I am a language learner, and I use a program called Anki that allows for 
-#importing text files of lists of words either seperated by space or comma
-#This is meant to save time, all I will eventually have to do is to give the language
-#and the word I am searching for once and it will be automatically added to the 
-#text file when adding words to something like a flash card file, you have to write them over
-#and over, and this seemed like a far simpler thing to do since I had some programming knowledge
+'''
+The lxml bits are not my own, however, the Scraper function is
+This was written in part with a website that had a tutorial on
+scraping with lxml, and I found it to be the easiest option I tried
+I am a language learner, and I use a program called Anki that allows for 
+importing text files of lists of words either seperated by space or comma
+This is meant to save time, all I will eventually have to do is to give the language
+and the word I am searching for once and it will be automatically added to the 
+text file when adding words to something like a flash card file, you have to write them over
+and over, and this seemed like a far simpler thing to do since I had some programming knowledge
+'''
 
 def Scraper(language, word):
 	#Will be the way you enter the language and the word
