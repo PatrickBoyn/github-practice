58f12671e377f27a7e519475076e52945aeebf4f
Patrick Boynton <dakilangmanok@gmail.com>
2017-09-11 00:36:57 -0400

    I got rid of all of my long comments, mostly to keep things simple. I decided I will write a README later explaining everything. That really is the place for the kinds of things I wanted to say.

--- WebScraper.py
+++ WebScraper.py
@@ -2,23 +2,7 @@ from lxml import html
 import requests
 
 
-'''
-The lxml bits are not my own, however, the Scraper function is
-This was written in part with a website that had a tutorial on
-scraping with lxml, and I found it to be the easiest option I tried
-I am a language learner, and I use a program called Anki that allows for 
-importing text files of lists of words either seperated by space or comma
-This is meant to save time, all I will eventually have to do is to give the language
-and the word I am searching for once and it will be automatically added to the 
-text file when adding words to something like a flash card file, you have to write them over
-and over, and this seemed like a far simpler thing to do since I had some programming knowledge
-'''
-
 def scraper(language, word):
-	#Will be the way you enter the language and the word
-	#you are looking for. en, then the abbreviation of the language
-	#prints the non english language first however if you want it to do it
-	#the other way around, simply switch en, and the other abrreviation
 	language
 	word
 
@@ -38,17 +22,13 @@ def scraper(language, word):
 	#prints the words you searched for
 	print(words[1]) 
 
-	works = scraper("enzh/", "cow")
-	f = open(r'C:\Users\dakil\Desktop\Chinese.txt', 'a')
-	f.write(works)
-	f.close()
 
 	
-# def printer():
-# 	works = scraper("enzh/", "cow")
-# 	f = open(r'C:\Users\dakil\Desktop\Chinese.txt', 'a')
-# 	f.write(works)
-	# f.close()
+def printer():
+ 	works = scraper("enzh/", "cow")
+ 	f = open(r'C:\Users\dakil\Desktop\Chinese.txt', 'a')
+ 	f.write(works)
+	f.close()
 
 try:
 	scraper("enzh/", "cow")
